4cf84d44e34d777eb299e15dd53cc00b4ee7aead
백승진 <bsj1048@naver.com>
2017-04-10 05:49:26 +0900

    add Calculator

---
+++ CalculatorWithEnum/src/main/java/com/riotgames/sample/CalcApp.java
@@ -0,0 +1,31 @@
+package com.riotgames.sample;
+
+import java.util.Arrays;
+
+/**
+ * Calculator application
+ */
+public class CalcApp {
+    public double calc(String[] tokens) {
+        final double firstOperand;
+        final double secondOperand;
+        firstOperand = Double.parseDouble(tokens[0]);
+        if (tokens.length > 2) {
+            secondOperand = Double.parseDouble(tokens[2]);
+        } else {
+            secondOperand = Double.parseDouble(tokens[1]);
+        }
+        final Operator operator = Operator.findOperator(tokens[1]);
+
+        return operator.evaluate(firstOperand, secondOperand);
+
+    }
+
+    public static void main( String[] args ) {
+        final CalcApp app = new CalcApp();
+        final StringBuilder outputs = new StringBuilder();
+        Arrays.asList(args).forEach(value -> outputs.append(value + " "));
+        System.out.print( "Addition of values: " + outputs + " = ");
+        System.out.println(app.calc(args));
+    }
+}
